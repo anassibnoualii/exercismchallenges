b0b10cbd371241300ae01ea8d50e02dfc19a641c
anass ibnou ali <a.ibnouali@insea.ac.ma>
2019-04-30 22:59:56 +0100

    [handshake] - handshake first atemp

--- secret-handshake/src/main/java/HandshakeCalculator.java
+++ secret-handshake/src/main/java/HandshakeCalculator.java
@@ -1,9 +1,19 @@
+import java.util.ArrayList;
 import java.util.List;
+
+import static java.util.Collections.reverse;
 
 class HandshakeCalculator {
 
     List<Signal> calculateHandshake(int number) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        List<Signal> signalList = new ArrayList<>();
+            if((number&1 )>0) signalList.add(Signal.WINK);
+            if((number&2 )>0) signalList.add(Signal.DOUBLE_BLINK);
+            if((number&4 )>0) signalList.add(Signal.CLOSE_YOUR_EYES);
+            if((number&8 )>0) signalList.add(Signal.JUMP);
+            if((number&16 )>0) reverse(signalList);
+
+        return signalList;
     }
 
 }
