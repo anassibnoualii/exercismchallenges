22ec6d1e16e6ff57f660a23256225a6e7daa9ba4
anass ibnou ali <a.ibnouali@insea.ac.ma>
2019-05-01 00:01:15 +0100

    [handshake] make reverse value static final

--- secret-handshake/src/main/java/HandshakeCalculator.java
+++ secret-handshake/src/main/java/HandshakeCalculator.java
@@ -5,9 +5,11 @@ import static java.util.Collections.reverse;
 
 class HandshakeCalculator {
 
+    private static final int REVERSE = 16;
+
     List<Signal> calculateHandshake(int number) {
         List<Signal> signalList =  Arrays.stream(Signal.values()).filter(signal -> (number & signal.getIndex()) > 0).collect(Collectors.toList());
-        if((number&16 )>0) reverse(signalList);
+        if((number & REVERSE)>0) reverse(signalList);
         return signalList;
     }
 
