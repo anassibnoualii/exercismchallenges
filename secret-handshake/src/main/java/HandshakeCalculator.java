77cd55cea9a8ee6b9efb5d19b53ffdb6d07402ca
anass ibnou ali <a.ibnouali@insea.ac.ma>
2019-04-30 23:50:20 +0100

    [handshake] - handshake with stream api

--- secret-handshake/src/main/java/HandshakeCalculator.java
+++ secret-handshake/src/main/java/HandshakeCalculator.java
@@ -1,18 +1,13 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static java.util.Collections.reverse;
 
 class HandshakeCalculator {
 
     List<Signal> calculateHandshake(int number) {
-        List<Signal> signalList = new ArrayList<>();
-            if((number&1 )>0) signalList.add(Signal.WINK);
-            if((number&2 )>0) signalList.add(Signal.DOUBLE_BLINK);
-            if((number&4 )>0) signalList.add(Signal.CLOSE_YOUR_EYES);
-            if((number&8 )>0) signalList.add(Signal.JUMP);
-            if((number&16 )>0) reverse(signalList);
-
+        List<Signal> signalList =  Arrays.stream(Signal.values()).filter(signal -> (number & signal.getIndex()) > 0).collect(Collectors.toList());
+        if((number&16 )>0) reverse(signalList);
         return signalList;
     }
 
--- secret-handshake/src/main/java/Signal.java
+++ secret-handshake/src/main/java/Signal.java
@@ -1,5 +1,13 @@
 enum Signal {
 
-    WINK, DOUBLE_BLINK, CLOSE_YOUR_EYES, JUMP
+    WINK(1), DOUBLE_BLINK(2), CLOSE_YOUR_EYES(4), JUMP(8);
+    int index;
 
+    Signal(int index) {
+        this.index=index;
+
+    }
+    public int getIndex(){
+        return index;
+    }
 }
