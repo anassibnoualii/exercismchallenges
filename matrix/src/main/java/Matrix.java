f76ba51b226c08c98521dd744eadfede17322c44
anass ibnou ali <a.ibnouali@insea.ac.ma>
2019-05-01 11:17:54 +0100

    [matrix] - adding matrix format validation  case

--- matrix/src/main/java/Matrix.java
+++ matrix/src/main/java/Matrix.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -13,12 +14,20 @@ class Matrix {
         internalMatrix = Stream.of(matrixAsString.split(ROWS_SPLITTER))
                 .map( Matrix::splitRow )
                 .collect(toList());
+        isValidMatrixFormat();
+
     }
 
     private static List<Integer> splitRow(String s) {
         return Stream.of(s.split(COLUMN_SPLITTER))
                 .map(Integer::valueOf)
                 .collect(toList());
+    }
+    private  void isValidMatrixFormat(){
+        for (List<Integer> integerList:internalMatrix) {
+            if(integerList.size()!=internalMatrix.get(0).size()) throw new IllegalArgumentException("Incorrect matrix format");
+        }
+
     }
 
     int[] getRow(int rowNumber) {
